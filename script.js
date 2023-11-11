45badc5981efd74c0026b29783fd2400c38b52b7
R7400 <142181578+R7400@users.noreply.github.com>
2023-11-10 20:32:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Step 1: Create a variable for the first number = (10).
+const number1 = 10 * 1; //Multiplication to get 10
+
+//Step 2:Create a variable for the second number = (40).
+const number2 = 80 - 40; //Subtraction to get 40
+
+//Step 3:Create a variable for the third number = (39).
+number3 = (80 - 70) + 29; //Subtraction and Addition to get 39
+
+//Combine the number with hyphens to form the combination.
+const combination = ${number1} - ${number2} - ${number3}
+
+//Display the combination in an alert popup.
+albert('The combination is: ${combination}');
+
+//Path: script.js    
